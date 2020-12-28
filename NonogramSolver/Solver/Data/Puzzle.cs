1e4cadd3754db2f256631b7da5d960904f2e54f6
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-28 12:12:41 +0100

    Add Data class

---
+++ NonogramSolver/Solver/Data/Puzzle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Solver.Data
+{
+    public class Puzzle
+    {
+        public List<List<uint>> Columns { get; set; }
+        public List<List<uint>> Rows { get; set; }
+        public int[,] Matrix { get; set; }
+    }
+}
