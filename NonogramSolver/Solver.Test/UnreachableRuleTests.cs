b805812f1a0f217771a4d6fa733e34f947702c14
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-28 17:15:23 +0100

    Add Unreachable Rule

---
+++ NonogramSolver/Solver.Test/UnreachableRuleTests.cs
@@ -0,0 +1,83 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver.Engine.Rules.Simple.Impl;
+
+namespace Solver.Test
+{
+    [TestClass]
+    public class UnreachableRuleTests
+    {
+        private readonly UnreachableRule unreachableRule = new UnreachableRule();
+
+        [TestMethod]
+        public void NoUnreachableMiddleTest()
+        {
+            uint number = 5;
+            int[] fields = new int[] { 0, 1, 1, 1, 0 };
+            int[] expected = new int[] { 0, 1, 1, 1, 0 };
+
+            var results = unreachableRule.Check(number, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NoUnreachableEndTest()
+        {
+            uint number = 5;
+            int[] fields = new int[] { 0, 0, 1, 1, 1 };
+            int[] expected = new int[] { 0, 0, 1, 1, 1 };
+
+            var results = unreachableRule.Check(number, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NoUnreachableStartTest()
+        {
+            uint number = 5;
+            int[] fields = new int[] { 1, 1, 1, 0, 0 };
+            int[] expected = new int[] { 1, 1, 1, 0 ,0  };
+
+            var results = unreachableRule.Check(number, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void UnreachableEndTest()
+        {
+            uint number = 3;
+            int[] fields = new int[] { 0, 1, 1, 0, 0, 0 };
+            int[] expected = new int[] { 0, 1, 1, 0, -1, -1 };
+
+            var results = unreachableRule.Check(number, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void UnreachableStartTest()
+        {
+            uint number = 3;
+            int[] fields = new int[] { 0, 0, 0, 1, 1, 0 };
+            int[] expected = new int[] { -1, -1, 0, 1, 1, 0 };
+
+            var results = unreachableRule.Check(number, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void UnreachableStartAndEndTest()
+        {
+            uint number = 3;
+            int[] fields = new int[] { 0, 0, 1, 1, 0, 0 };
+            int[] expected = new int[] { -1, 0, 1, 1, 0, -1 };
+
+            var results = unreachableRule.Check(number, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ NonogramSolver/Solver/Rules/Simple/Impl/UnreachableRule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Solver.Engine.Rules.Simple.Impl
+{
+    public class UnreachableRule : ISimpleRule
+    {
+        public int[] Check(uint number, int[] fields)
+        {
+            int? max = null;
+            int? min = null;
+
+            for(int i = 0; i < fields.Length; i++)
+            {
+                if (fields[i] > 0)
+                {
+                    max = i;
+
+                    if (min == null)
+                    {
+                        min = i;
+                    }
+                }
+            }
+
+            if(max != null && min != null)
+            {
+                return Fill(min.GetValueOrDefault(), max.GetValueOrDefault(), number, fields);
+            }
+
+            return fields;
+        }
+
+        private int[] Fill(int min, int max, uint number, int[] fields)
+        {
+            long maxDistance = number - (max - min);
+            if(maxDistance <= 0)
+            {
+                return fields;
+            }
+
+            for(int i = 0; i < fields.Length; i++)
+            {
+                if(Math.Abs(i - min) >= maxDistance && Math.Abs(i - max) >= maxDistance)
+                {
+                    fields[i] = -1;
+                }
+            }
+
+            return fields;
+        }
+    }
+}
