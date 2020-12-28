430f51fa5aefef6a71e4b4093718228b23e7b1d5
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-28 13:42:43 +0100

    Add SummRule

---
+++ NonogramSolver/Solver.Test/SummRuleTests.cs
@@ -0,0 +1,84 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver.Engine.Rules.Implementations;
+using System.Collections.Generic;
+
+namespace Solver.Test
+{
+    [TestClass]
+    public class SummRuleTests
+    {
+        private readonly SummRule summRule = new SummRule();
+
+        [TestMethod]
+        public void FullWithOneNumberTest()
+        {
+            List<uint> numbers = new List<uint>{ 5 };
+            int[] fields = new int[5];
+            int[] expected = new int[] { 1, 1, 1, 1, 1 };
+
+            var results = summRule.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void FullWithTwoNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 2, 2 };
+            int[] fields = new int[5];
+            int[] expected = new int[] { 1, 1, -1, 1, 1 };
+
+            var results = summRule.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void FullWithMoreNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 1, 1, 1 };
+            int[] fields = new int[5];
+            int[] expected = new int[] { 1, -1, 1, -1, 1 };
+
+            var results = summRule.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NotFullWithOneNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 4 };
+            int[] fields = new int[5];
+            int[] expected = new int[5];
+
+            var results = summRule.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NotFullWithTwoNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 2, 1 };
+            int[] fields = new int[5];
+            int[] expected = new int[5];
+
+            var results = summRule.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NotFullWithMoreNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 2, 1, 1 };
+            int[] fields = new int[7];
+            int[] expected = new int[7];
+
+            var results = summRule.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ NonogramSolver/Solver/Rules/Implementations/SummRule.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Solver.Engine.Rules.Implementations
+{
+    public class SummRule : IRule
+    {
+        public int[] Check(List<uint> numbers, int[] fields)
+        {
+            long summ = numbers.Count - 1;
+            foreach(var number in numbers)
+            {
+                summ += number;
+            }
+
+            if(summ == fields.Length)
+            {
+                return Fill(numbers, fields);
+            }
+
+            return fields;
+        }
+
+        private int[] Fill(List<uint> numbers, int[] fields)
+        {
+            int index = 0;
+            foreach(var number in numbers)
+            {
+                for(uint i = 0;  i < number; i++)
+                {
+                    fields[index] = 1;
+                    index++;
+                }
+                if(index != fields.Length)
+                {
+                    fields[index] = -1;
+                    index++;
+
+                }
+            }
+
+            return fields;
+        }
+    }
+}
