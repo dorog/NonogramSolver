472c7b731e5f8fac9d8826a263fc83e2ffd5ad3b
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-28 13:09:01 +0100

    Add MoreThanHalf Rule

---
+++ NonogramSolver/Solver.Test/MoreThanHalfRuleTests.cs
@@ -0,0 +1,72 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver.Engine.Rules.HalfOrMore;
+using System.Collections.Generic;
+
+namespace Solver.Test
+{
+    [TestClass]
+    public class MoreThanHalfRuleTests
+    {
+        private readonly MoreThanHalf halfOrMore = new MoreThanHalf();
+
+        [TestMethod]
+        public void FullTest()
+        {
+            List<uint> numbers = new List<uint>{ 5 };
+            int[] fields = new int[5];
+            int[] expected = new int[] { 1, 1, 1, 1, 1 };
+
+            var results = halfOrMore.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void MoreThanHalfTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[5];
+            int[] expected = new int[] { 0, 0, 1, 0, 0 };
+
+            var results = halfOrMore.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void HalfTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[6];
+            int[] expected = new int[] { 0, 0, 0, 0, 0, 0 };
+
+            var results = halfOrMore.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void LessThanHalfTest()
+        {
+            List<uint> numbers = new List<uint> { 2 };
+            int[] fields = new int[6];
+            int[] expected = new int[] { 0, 0, 0, 0, 0, 0 };
+
+            var results = halfOrMore.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NotEmpyRowAndMoreThanHalfTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[5] { 0, 0, 0, 0, 1 };
+            int[] expected = new int[] { 0, 0, 1, 0, 1 };
+
+            var results = halfOrMore.Check(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ NonogramSolver/Solver/Rules/MoreThanHalf/MoreThanHalf.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Solver.Engine.Rules.HalfOrMore
+{
+    public class MoreThanHalf : IRule
+    {
+        public int[] Check(List<uint> numbers, int[] fields)
+        {
+            if (numbers.Count == 1)
+            {
+                return Fill(numbers[0], fields);
+            }
+            else
+            {
+                return fields;
+            }
+        }
+
+        private int[] Fill(uint number, int[] fields)
+        {
+            if(number > fields.Length / 2)
+            {
+                for(int i = 0; i < fields.Length; i++)
+                {
+                    if (i < number && fields.Length - number <= i)
+                    {
+                        fields[i] = 1;
+                    }
+                }
+            }
+
+            return fields;
+        }
+    }
+}
