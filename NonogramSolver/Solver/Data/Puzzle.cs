95b0bad2bf47f410504da618f2228acf70328e65
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-29 10:51:57 +0100

    Add Solver Engine first version

--- NonogramSolver/Solver/Data/Puzzle.cs
+++ NonogramSolver/Solver/Data/Puzzle.cs
@@ -7,5 +7,45 @@ namespace Solver.Data
         public List<List<uint>> Columns { get; set; }
         public List<List<uint>> Rows { get; set; }
         public int[,] Matrix { get; set; }
+
+        public int[] GetMatrixRow(int index)
+        {
+            int[] row = new int[Matrix.GetLength(1)];
+
+            for(int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                row[i] = Matrix[index, i];
+            }
+
+            return row;
+        }
+
+        public void SetMatrixRow(int index, int[] row)
+        {
+            for (int i = 0; i < Matrix.GetLength(1); i++)
+            {
+                Matrix[index, i] = row[i];
+            }
+        }
+
+        public int[] GetMatrixColumn(int index)
+        {
+            int[] column = new int[Matrix.GetLength(0)];
+
+            for (int i = 0; i < Matrix.GetLength(1); i++)
+            {
+                column[i] = Matrix[i, index];
+            }
+
+            return column;
+        }
+
+        public void SetMatrixColumn(int index, int[] column)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                Matrix[i, index] = column[i];
+            }
+        }
     }
 }
---
+++ NonogramSolver/Solver/SolverEngine.cs
@@ -0,0 +1,77 @@
+﻿using Solver.Data;
+using Solver.Engine.Rules;
+using Solver.Engine.Rules.Complex.Impl;
+using Solver.Engine.Rules.Simple;
+using Solver.Engine.Rules.Simple.Impl;
+using System.Collections.Generic;
+
+namespace Solver.Engine
+{
+    public static class SolverEngine
+    {
+        private static readonly List<IComplexRule> complexRules = new List<IComplexRule>()
+        {
+            new DoneRule()
+        };
+
+        private static readonly List<ISimpleRule> simpleRules = new List<ISimpleRule>()
+        {
+            new MergeRule(),
+            new MoreThanHalfRule(),
+            new UnreachableRule(),
+        };
+
+        public static Puzzle Solve(Puzzle puzzle)
+        {
+            if (FinishedPuzzle(puzzle))
+            {
+                return puzzle;
+            }
+
+            ApplyComplexRuleOnThePuzzle(puzzle, new SummRule());
+
+            while (!FinishedPuzzle(puzzle))
+            {
+                foreach(var complexRule in complexRules)
+                {
+                    ApplyComplexRuleOnThePuzzle(puzzle, complexRule);
+                }
+            }
+
+            return puzzle;
+        }
+
+        private static bool FinishedPuzzle(Puzzle puzzle)
+        {
+            for(int i = 0; i < puzzle.Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < puzzle.Matrix.GetLength(1); j++)
+                {
+                    if (puzzle.Matrix[i, j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static void ApplyComplexRuleOnThePuzzle(Puzzle puzzle, IComplexRule complexRule)
+        {
+            for (int i = 0; i < puzzle.Matrix.GetLength(0); i++)
+            {
+                int[] actualRow = puzzle.GetMatrixRow(i);
+                int[] calculatedRow = complexRule.Check(puzzle.Rows[i], actualRow);
+                puzzle.SetMatrixRow(i, calculatedRow);
+            }
+
+            for (int i = 0; i < puzzle.Matrix.GetLength(1); i++)
+            {
+                int[] actualColumn = puzzle.GetMatrixColumn(i);
+                int[] calculatedRow = complexRule.Check(puzzle.Columns[i], actualColumn);
+                puzzle.SetMatrixColumn(i, calculatedRow);
+            }
+        }
+    }
+}
