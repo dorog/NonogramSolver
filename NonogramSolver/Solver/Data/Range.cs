29a87de1568e6f7574feceab35ab996ef664d259
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-29 11:12:43 +0100

    Add Range Data class

---
+++ NonogramSolver/Solver/Data/Range.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Solver.Engine.Data
+{
+    public class Range
+    {
+        public bool Done { get; set; } = false;
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+}
