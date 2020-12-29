629c5d67244571dcd4e3bbec4d839725d7659dea
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-29 23:00:51 +0100

    Add End Separator

---
+++ NonogramSolver/Solver.Test/SeparatorTests/EndSeparatorTests.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver.Engine.Data;
+using Solver.Engine.Separators.Impl;
+using System.Collections.Generic;
+
+namespace Solver.Test.SeparatorTests
+{
+    [TestClass]
+    public class EndSeparatorTests
+    {
+        private readonly EndSeparator endSeparator = new EndSeparator();
+
+        [TestMethod]
+        public void NoWhiteFieldWithOneNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 1, 1, 1, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null   
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void NoWhiteFieldWithMoreNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 3, 1 };
+            int[] fields = new int[] { 0, 1, 1, 1, 0, 1 };
+            Range[] expected = new Range[]
+            {
+                null,
+                null,
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberButWithoutSolidTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 0, 0, -1, 0, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberWithSolidStartPerfectTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 1, 0, -1, 0, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                new Range()
+                {
+                    Start = 0,
+                    End = 2,
+                }
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberWithSolidStartGreaterWithOneTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 1, 0, 0, 0, -1, 0, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                new Range()
+                {
+                    Start = 0,
+                    End = 3,
+                }
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberWithSolidStartGreaterWithMoreTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 1, 0, 0, 0, -1, 0, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberWithSolidEndPerfectTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 0, 0, -1, 0, 0, 1 };
+            Range[] expected = new Range[]
+            {
+                new Range()
+                {
+                    Start = 4,
+                    End = 6,
+                }
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberWithSolidEndGreaterWithOneTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 0, 0, -1, 1, 0, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                new Range()
+                {
+                    Start = 4,
+                    End = 7,
+                }
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void WhiteFieldWithOneNumberWithSolidEndGreaterWithMoreTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 0, 0, -1, 0, 1, 0, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null
+            };
+
+            var results = endSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ NonogramSolver/Solver/Separators/Impl/EndSeparator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Solver.Engine.Data;
+
+namespace Solver.Engine.Separators.Impl
+{
+    public class EndSeparator : ISeparator
+    {
+        public Range[] Separate(List<uint> numbers, int[] fields)
+        {
+            Range[] ranges = new Range[numbers.Count];
+
+            int? firstWhite = null;
+            int? lastWhite = null;
+
+            for(int i = 0; i < fields.Length; i++)
+            {
+                if(fields[i] == -1)
+                {
+                    lastWhite = i;
+
+                    if(firstWhite == null)
+                    {
+                        firstWhite = i;
+                    }
+                }
+            }
+
+            if(lastWhite != null)
+            {
+                int lastWhiteLocation = lastWhite.GetValueOrDefault();
+                int lastFieldIndex = fields.Length - 1;
+                if (lastFieldIndex - lastWhiteLocation <= numbers[^1] + 1 && lastFieldIndex - lastWhiteLocation >= numbers[^1] && SolidExists(lastWhiteLocation, lastFieldIndex + 1, fields))
+                {
+                    ranges[0] = new Range()
+                    {
+                        Start = (uint)(lastWhiteLocation + 1),
+                        End = (uint)fields.Length - 1
+                    };
+                }
+            }
+
+            if(firstWhite != null)
+            {
+                int firstWhiteLocation = firstWhite.GetValueOrDefault();
+                if (firstWhiteLocation <= numbers[0] + 1 && firstWhiteLocation >= numbers[0] && SolidExists(0, firstWhiteLocation, fields))
+                {
+                    ranges[0] = new Range()
+                    {
+                        Start = 0,
+                        End = (uint)firstWhiteLocation - 1
+                    };
+                }
+            }
+
+            return ranges;
+        }
+
+        private bool SolidExists(int min, int max, int[] fields)
+        {
+            for(int i = min; i < max; i++)
+            {
+                if(fields[i] == 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
