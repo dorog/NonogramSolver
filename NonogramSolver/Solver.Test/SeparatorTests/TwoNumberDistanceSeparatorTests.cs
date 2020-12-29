6b486a8b15399564100690aba7226a71b1259270
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-29 18:12:36 +0100

    Add TwoNumberDistance Separator

---
+++ NonogramSolver/Solver.Test/SeparatorTests/TwoNumberDistanceSeparatorTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver.Engine.Data;
+using Solver.Engine.Separators.Impl;
+using System.Collections.Generic;
+
+namespace Solver.Test.SeparatorTests
+{
+    [TestClass]
+    public class TwoNumberDistanceSeparatorTests
+    {
+        private readonly TwoNumberDistanceSeparator twoNumberDistanceSeparator = new TwoNumberDistanceSeparator();
+
+        [TestMethod]
+        public void OneNumberRangeTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 1, 1, 1, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null
+            };
+
+            var results = twoNumberDistanceSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void MoreThanTwoNumberRangeTest()
+        {
+            List<uint> numbers = new List<uint> { 1, 1, 1 };
+            int[] fields = new int[] { 0, 1, 0, 1, 0, 1 };
+            Range[] expected = new Range[]
+            {
+                null,
+                null,
+                null
+            };
+
+            var results = twoNumberDistanceSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void TwoNumberTwoSolidButTooCloseRangeTest()
+        {
+            List<uint> numbers = new List<uint> { 1, 2 };
+            int[] fields = new int[] { 0, 1, 0, 1, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null,
+                null
+            };
+
+            var results = twoNumberDistanceSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void TwoNumberMoreSolidButTooCloseRangeTest()
+        {
+            List<uint> numbers = new List<uint> { 1, 3 };
+            int[] fields = new int[] { 0, 1, 0, 1, 0, 1 };
+            Range[] expected = new Range[]
+            {
+                null,
+                null
+            };
+
+            var results = twoNumberDistanceSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void TwoNumberTwoSolidAndEnoughSpaceRangeTest()
+        {
+            List<uint> numbers = new List<uint> { 1, 1 };
+            int[] fields = new int[] { 0, 1, 0, 0, 1, 0 };
+            Range[] expected = new Range[]
+            {
+                new Range(){ Start = 0, End = 2 },
+                new Range(){ Start = 3, End = 5 }
+            };
+
+            var results = twoNumberDistanceSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void TwoNumberMoreSolidAndEnoughSpaceRangeTest()
+        {
+            List<uint> numbers = new List<uint> { 1, 3 };
+            int[] fields = new int[] { 0, 1, 0, 0, 0, 1, 0, 1 };
+            Range[] expected = new Range[]
+            {
+                new Range(){ Start = 0, End = 2 },
+                new Range(){ Start = 3, End = 7 },
+            };
+
+            var results = twoNumberDistanceSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ NonogramSolver/Solver/Separators/Impl/TwoNumberDistanceSeparator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Solver.Engine.Data;
+
+namespace Solver.Engine.Separators.Impl
+{
+    public class TwoNumberDistanceSeparator : ISeparator
+    {
+        public Range[] Separate(List<uint> numbers, int[] fields)
+        {
+            Range[] ranges = new Range[numbers.Count];
+            if(numbers.Count == 2)
+            {
+                uint max = numbers[0] + numbers[1];
+                List<int> solidPositions = new List<int>();
+                for(int i = 0; i < fields.Length; i++)
+                {
+                    if(fields[i] == 1)
+                    {
+                        solidPositions.Add(i);
+                    }
+                }
+
+                for(int i = 1; i < solidPositions.Count; i++)
+                {
+                    if(solidPositions[i] - solidPositions[i-1] >= max)
+                    {
+                        uint middle = (uint) (solidPositions[i - 1] + numbers[0]);
+                        ranges[0] = new Range()
+                        {
+                            Start = 0,
+                            End = middle
+                        };
+                        ranges[1] = new Range()
+                        {
+                            Start = middle + 1,
+                            End = (uint)fields.Length - 1
+                        };
+
+                        return ranges;
+                    }
+                }
+            }
+
+            return ranges;
+        }
+    }
+}
