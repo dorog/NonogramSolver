08968e629345222724aafd8dbb796050d76cd49e
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2020-12-29 21:02:07 +0100

    Add OnlyOneNumber Separator

---
+++ NonogramSolver/Solver.Test/SeparatorTests/OnlyOneNumberSeparatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver.Engine.Data;
+using Solver.Engine.Separators.Impl;
+using System.Collections.Generic;
+
+namespace Solver.Test.SeparatorTests
+{
+    [TestClass]
+    public class OnlyOneNumberSeparatorTests
+    {
+        private readonly OnlyOneNumberSeparator onlyOneNumberSeparator = new OnlyOneNumberSeparator();
+
+        [TestMethod]
+        public void OneNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 3 };
+            int[] fields = new int[] { 0, 1, 1, 1, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                new Range()
+                {
+                    Start = 0,
+                    End = 5
+                }
+            };
+
+            var results = onlyOneNumberSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+
+
+        [TestMethod]
+        public void TwoNumberTest()
+        {
+            List<uint> numbers = new List<uint> { 3, 1 };
+            int[] fields = new int[] { 0, 1, 1, 1, 0, 0 };
+            Range[] expected = new Range[]
+            {
+                null,
+                null
+            };
+
+            var results = onlyOneNumberSeparator.Separate(numbers, fields);
+
+            CollectionAssert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ NonogramSolver/Solver/Separators/Impl/OnlyOneNumberSeparator.cs
@@ -0,0 +1,23 @@
+﻿using Solver.Engine.Data;
+using System.Collections.Generic;
+
+namespace Solver.Engine.Separators.Impl
+{
+    public class OnlyOneNumberSeparator : ISeparator
+    {
+        public Range[] Separate(List<uint> numbers, int[] fields)
+        {
+            Range[] ranges = new Range[numbers.Count];
+            if(numbers.Count == 1)
+            {
+                ranges[0] = new Range()
+                {
+                    Start = 0,
+                    End = (uint)fields.Length - 1
+                };
+            }
+
+            return ranges;
+        }
+    }
+}
