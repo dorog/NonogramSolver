fae6b68f071badc63bc0ae972a23e7f02c4579f6
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2021-01-01 17:11:04 +0100

    Remove Range unnecessary Done property

--- NonogramSolver/Solver/Data/Range.cs
+++ NonogramSolver/Solver/Data/Range.cs
@@ -4,21 +4,19 @@ namespace Solver.Engine.Data
 {
     public class Range
     {
-        public bool Done { get; set; } = false;
         public int Start { get; set; }
         public int End { get; set; }
 
         public override bool Equals(object obj)
         {
             return obj is Range range &&
-                   Done == range.Done &&
                    Start == range.Start &&
                    End == range.End;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Done, Start, End);
+            return HashCode.Combine(Start, End);
         }
     }
 }
