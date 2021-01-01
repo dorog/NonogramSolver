7f5e6cc53b05a0f328e81777397209b525880283
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2021-01-01 19:39:47 +0100

    Refactor: TwoNumberDistance Separator

--- NonogramSolver/Solver/Separators/Impl/TwoNumberDistanceSeparator.cs
+++ NonogramSolver/Solver/Separators/Impl/TwoNumberDistanceSeparator.cs
@@ -11,28 +11,22 @@ namespace Solver.Engine.Separators.Impl
             if(numbers.Count == 2)
             {
                 int max = numbers[0] + numbers[1];
-                List<int> solidPositions = new List<int>();
-                for(int i = 0; i < fields.Length; i++)
-                {
-                    if(fields[i] == FieldType.Solid)
-                    {
-                        solidPositions.Add(i);
-                    }
-                }
+
+                List<int> solidPositions = GetSolidPositions(fields);
 
                 for(int i = 1; i < solidPositions.Count; i++)
                 {
                     if(solidPositions[i] - solidPositions[i-1] >= max)
                     {
-                        int middle = solidPositions[i - 1] + numbers[0];
+                        int separatorFieldIndex = solidPositions[i - 1] + numbers[0];
                         ranges[0] = new Range()
                         {
                             Start = 0,
-                            End = middle
+                            End = separatorFieldIndex
                         };
                         ranges[1] = new Range()
                         {
-                            Start = middle + 1,
+                            Start = separatorFieldIndex + 1,
                             End = fields.Length - 1
                         };
 
@@ -42,6 +36,20 @@ namespace Solver.Engine.Separators.Impl
             }
 
             return ranges;
+        }
+
+        private List<int> GetSolidPositions(FieldType[] fields)
+        {
+            List<int> solidPositions = new List<int>();
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (fields[i] == FieldType.Solid)
+                {
+                    solidPositions.Add(i);
+                }
+            }
+
+            return solidPositions;
         }
     }
 }
